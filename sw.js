0e04da4c48be4021d2b24e98c166922e701f9353
Annonymous-pkg <115738685+Annonymous-pkg@users.noreply.github.com>
2026-01-06 20:05:10 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+
+const CACHE_NAME = 'culina-prestige-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './index.tsx',
+  'https://cdn-icons-png.flaticon.com/512/1046/1046747.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('./index.html') || caches.match('./'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
